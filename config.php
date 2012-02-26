296c8c5ff89bdb32be01c8eb136b9b7efdfb59da
Craig Patik <cpatik@gmail.com>
2012-02-26 15:50:43 -0500

    Initial customization for my personal domain

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
 
 define('MYSQL_HOST', 'localhost');
-define('MYSQL_USER', 'user');
-define('MYSQL_PASSWORD', 'password');
-define('MYSQL_DATABASE', 'database');
-define('TWITTER_USERNAME', 'mathias');
-define('GOOGLE_PLUS_ID', '106697091536876736486');
-define('SHORT_URL', 'http://mths.be/'); // include the trailing slash!
-define('DEFAULT_URL', 'http://mathiasbynens.be'); // omit the trailing slash!
+define('MYSQL_USER', 'xxxxxxxxxxx');
+define('MYSQL_PASSWORD', 'xxxxxxxxxxx');
+define('MYSQL_DATABASE', 'xxxxxxxxxxx');
+define('TWITTER_USERNAME', 'craigpatik');
+define('GOOGLE_PLUS_ID', '116513687533678150554');
+define('SHORT_URL', 'http://crgp.tk/'); // include the trailing slash!
+define('DEFAULT_URL', 'http://patik.com'); // omit the trailing slash!
 
 ?>
