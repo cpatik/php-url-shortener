362e075dcb6fd29eabf0965f0daf5db9665eb1e4
Craig Patik <cpatik@gmail.com>
2012-02-26 16:41:26 -0500

    HTML output when navigated directly
    Added HTML output when navigated to directly. The bookmarklet still receives a plaintext response

--- shorten.php
+++ shorten.php
@@ -2,12 +2,14 @@
 
 require 'config.php';
 
-header('Content-Type: text/plain;charset=UTF-8');
-
 $url = isset($_GET['url']) ? urldecode(trim($_GET['url'])) : '';
+$isBookmarklet = (isset($_GET['bm']) && strlen($_GET['bm']) > 0) ? true : false;
+$longurl = $url;
+$response = "";
+$success = false;
 
 if (in_array($url, array('', 'about:blank', 'undefined', 'http://localhost/'))) {
-	die('Enter a URL.');
+	$response = "Enter a URL.";
 }
 
 function nextLetter(&$str) {
@@ -34,17 +36,103 @@ $url = $db->real_escape_string($url);
 
 $result = $db->query('SELECT slug FROM redirect WHERE url = "' . $url . '" LIMIT 1');
 if ($result && $result->num_rows > 0) { // If there’s already a short URL for this URL
-	die(SHORT_URL . $result->fetch_object()->slug);
-} else {
+	$response = SHORT_URL . $result->fetch_object()->slug;
+	$success = true;
+}
+else {
 	$result = $db->query('SELECT slug, url FROM redirect ORDER BY date DESC LIMIT 1');
 	if ($result && $result->num_rows > 0) {
 		$slug = getNextShortURL($result->fetch_object()->slug);
 		if ($db->query('INSERT INTO redirect (slug, url, date, hits) VALUES ("' . $slug . '", "' . $url . '", NOW(), 0)')) {
+			$response = SHORT_URL . $slug;
+			$success = true;
 			header('HTTP/1.1 201 Created');
-			echo SHORT_URL . $slug;
 			$db->query('OPTIMIZE TABLE `redirect`');
 		}
 	}
 }
 
+# Display result
+
+if ($isBookmarklet) {
+	header('Content-Type: text/plain;charset=UTF-8');
+	echo $response;
+}
+else {
+	header('Content-Type: text/html;charset=UTF-8');
+	?>
+	<!doctype html>
+	<html>
+	<head>
+		<title>crgp.tk URL Shortener</title>
+		<style>
+		body {
+			font: 1.2em/2em "Myriad Pro", "Helvetica Neue", Helvetica, "Segoe UI", sans-serif;
+			color: #444;
+			max-width: 32em;
+			margin: 1em auto;
+			background-color: #f4f4f4;
+		}
+		input, a {
+			font: 1em/1.8em Menlo, Consolas, "Courier New", monospace;
+		}
+		input {
+			color: #444;
+			margin-left: 0.5em;
+		}
+		a {
+			color: #0086B3;
+			border-bottom: 1px solid transparent;
+			text-decoration: none;
+			-webkit-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+			   -moz-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+			    -ms-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+			     -o-transition: color 0.2s ease, border-bottom-color 0.2s ease;
+			        transition: color 0.2s ease, border-bottom-color 0.2s ease;
+		}
+		a:hover,
+		a:focus {
+			color: #00B9F7;
+			border-bottom: 1px solid #00B9F7;
+		}
+		.orig {
+			font-size: smaller;
+		}
+		.orig, .orig a {
+			color: #999;
+		}
+		</style>
+	</head>
+	<body>
+	<?php
+	if ($success) {
+		?>
+		<p>Short URL: <a href="<?php echo $response; ?>"><?php echo $response; ?></a></p>
+		<p>Copy: <input id="urlInput" type="url" size="<?php echo (strlen($response) + 3); ?>" value="<?php echo $response; ?>"></p>
+		<p class="orig">Original URL: <a href="<?php echo $longurl; ?>"><?php echo $longurl; ?></a></p>
+		<?php
+	}
+	else {
+		?>
+		<p><?php echo $response; ?></p>
+		<?php
+	}
+	?>
+		<script>
+		function setFocus() {
+			var input = document.getElementById("urlInput");
+			input.focus();
+			try { input.select(); } catch(e) { }
+		}
+		if (window.addEventListener) {
+			window.addEventListener("load", setFocus, false);
+		}
+		else if (window.attachEvent) {
+			window.attachEvent("onload", setFocus);
+		}
+		</script>
+	</body>
+	</html>
+	<?php
+}
 ?>
